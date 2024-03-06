d546aad55cc1a0250b42ba550cc213c7ffd7b615
lyes <bob.lyes@live.com>
2024-03-06 22:16:46 +0100

    make test fail

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,7 +7,7 @@ describe('login spec', () => {
     cy.get('[data-test="username"]').type('standard_user')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('[data-test="login-button"]').should('be.visible').and('be.enabled').click()
-    cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
+    cy.url().should('eq', 'https://www.saucedemo.com/inventory.htmlmmmm')
   })
 
 it('login failed', () => {
